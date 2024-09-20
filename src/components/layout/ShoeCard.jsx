9dab0d94f0ce5b4b64520172ebe0f5b01496d69c
ClementMadiot <clementmadiot09@gmail.com>
2024-09-20 14:29:38 +0200

    shoe card with effect

--- src/components/layout/ShoeCard.jsx
+++ src/components/layout/ShoeCard.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function ShoeCard({imgURL, bigShoeImg, changeBigShoe}) {
+function ShoeCard({ imgURL, bigShoeImg, changeBigShoe }) {
   const handleClick = () => {
-    if(bigShoeImg !== imgURL) {
-      changeBigShoe(imgURL.bigShoe)
+    if (bigShoeImg !== imgURL) {
+      changeBigShoe(imgURL.bigShoe);
     }
-  }
+  };
   return (
-    <div className={`border-2 rounded-xl ${bigShoeImg === imgURL ? 'border-coral-red' : 'border-transparent'} cursor-pointer max-sm:flex-1`}
-    onClick={handleClick}
+    <div
+      className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 hover:scale-105 duration-150 ${
+        bigShoeImg === imgURL.bigShoe 
+        ? "border-coral-red hover:border-coral-red" 
+        : "border-transparent"
+      }`}
+      onClick={handleClick}
     >
-
+      <div className="flex justify-center items-center bg-card bg-center sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img src={imgURL.thumbnail} alt={`shoe collection ${imgURL.thumbnail}`} width={127} height={103} className="object-contain" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ShoeCard
+export default ShoeCard;
